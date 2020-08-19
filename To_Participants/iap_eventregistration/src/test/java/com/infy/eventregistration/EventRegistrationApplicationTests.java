551e7c6e8692848de933fb6a631a454edd63ab65
Sagar Hiremath <sagar.hiremath@ad.infosys.com>
2020-08-19 20:08:56 +0530

    Updated QP

--- To_Participants/iap_eventregistration/src/test/java/com/infy/eventregistration/EventRegistrationApplicationTests.java
+++ To_Participants/iap_eventregistration/src/test/java/com/infy/eventregistration/EventRegistrationApplicationTests.java
@@ -1,20 +1,5 @@
 package com.infy.eventregistration;
 
-import com.infy.eventregistration.repository.EventRepository;
-import com.infy.eventregistration.repository.ParticipantRepository;
-import com.infy.eventregistration.service.EventService;
-import com.infy.eventregistration.service.EventServiceImpl;
-
 public class EventRegistrationApplicationTests {
-	
-	private EventRepository eventRepository;
-	
-	private ParticipantRepository participantRepository;
-	
-	private EventService eventService = new EventServiceImpl();
-	
-	public void getParticipantsByEventVenueNoParticipantsFoundTest() {
-		
-	}
 	
 }
