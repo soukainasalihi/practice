8fa4845b2fda157a8e02c535da0bbbe1e7aa2f67
Sagar Hiremath <sagar.hiremath@ad.infosys.com>
2020-08-19 16:53:14 +0530

    Updated QP and test cases

--- To_Participants/iap_eventregistration/src/main/java/com/infy/eventregistration/repository/ParticipantRepository.java
+++ To_Participants/iap_eventregistration/src/main/java/com/infy/eventregistration/repository/ParticipantRepository.java
@@ -1,4 +1,9 @@
 package com.infy.eventregistration.repository;
 
+import java.util.List;
+
+import com.infy.eventregistration.entity.Participant;
+
 public interface ParticipantRepository {
+	List<Participant> getParticipantsByEventVenue(String venue);
 }
